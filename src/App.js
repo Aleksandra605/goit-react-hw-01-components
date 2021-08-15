5c017462ccfdd8e4a0fa95bda39fc04a7f01805f
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2021-08-15 17:23:08 +0200

    just started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import statisticalData from './component-2/statistical-data.json';
+import Statistics from './component-2/statistics';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return;
 }
 
 export default App;
