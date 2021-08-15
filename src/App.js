618a79677715093fa775342946f5e1e33db566bc
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2021-08-15 16:59:04 +0200

    works without styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './component-1/Profile';
+import user from './data/user.json';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Profile
+      userImage={user.avatar}
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    />
   );
 }
-
-export default App;
---
+++ src/component-1/Profile.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+
+export default function UserProfile(props) {
+  const { userImage, name, tag, location, followers, views, likes } = props;
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={userImage} alt="Аватар пользователя" class="avatar" />
+        <p class="name">{name}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers: </span>
+          <span class="quantity">{followers}</span>
+        </li>
+        <li>
+          <span class="label">Views: </span>
+          <span class="quantity">{views}</span>
+        </li>
+        <li>
+          <span class="label">Likes: </span>
+          <span class="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+UserProfile.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  followers: PropTypes.number,
+  Views: PropTypes.number,
+  likes: PropTypes.number,
+};
